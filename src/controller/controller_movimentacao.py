fbb405f711a8d93021beb95d1c7dcb376a1dfe88
Jorge Donato <jorge.donato@live.com>
2023-10-17 01:22:07 +0100

    Alterando controller_movimentacao

--- src/controller/controller_movimentacao.py
+++ src/controller/controller_movimentacao.py
@@ -16,59 +16,33 @@ class Controller_Movimentacao:
         # Lista os pedido existentes para inserir no item de pedido
         self.listar_contas(oracle, need_connect=True)
         lnumero = int(input("Digite o número da Conta: "))
-        pedido = self.valida_pedido(oracle, lnumero)
-        if pedido == None:
+        nidconta = self.valida_conta(oracle, lnumero)
+        if nidconta == None:
             return None
 
-        # Lista os produtos existentes para inserir no item de pedido
-        self.listar_produtos(oracle, need_connect=True)
-        codigo_produto = str(input("Digite o código do Produto: "))
-        produto = self.valida_produto(oracle, codigo_produto)
-        if produto == None:
-            return None
-
-        # Solicita a quantidade de itens do pedido para o produto selecionado
-        quantidade = float(input(f"Informe a quantidade de itens do produto {produto.get_descricao()}: "))
-        # Solicita o valor unitário do produto selecionado
-        valor_unitario = float(input(f"Informe o valor unitário do produto {produto.get_descricao()}: "))
-
-        # Recupera o cursor para executar um bloco PL/SQL anônimo
-        cursor = oracle.connect()
-        # Cria a variável de saída com o tipo especificado
-        output_value = cursor.var(int)
-
-        # Cria um dicionário para mapear as variáveis de entrada e saída
-        data = dict(codigo=output_value, quantidade=quantidade, valor_unitario=valor_unitario, codigo_pedido=int(pedido.get_codigo_pedido()), codigo_produto=int(produto.get_codigo()))
-        # Executa o bloco PL/SQL anônimo para inserção do novo item de pedido e recuperação da chave primária criada pela sequence
-        cursor.execute("""
-        begin
-            :codigo := ITENS_PEDIDO_CODIGO_ITEM_SEQ.NEXTVAL;
-            insert into itens_pedido values(:codigo, :quantidade, :valor_unitario, :codigo_pedido, :codigo_produto);
-        end;
-        """, data)
         # Recupera o código do novo item de pedido
-        codigo_item_pedido = output_value.getvalue()
+        codigo_movimentacao = output_value.getvalue()
         # Persiste (confirma) as alterações
         oracle.conn.commit()
         # Recupera os dados do novo item de pedido criado transformando em um DataFrame
-        df_item_pedido = oracle.sqlToDataFrame(f"select codigo_item_pedido, quantidade, valor_unitario, codigo_pedido, codigo_produto from itens_pedido where codigo_item_pedido = {codigo_item_pedido}")
+        df_movimentacao = oracle.sqlToDataFrame(f"select codigo_movimentacao, quantidade, valor_unitario, codigo_pedido, codigo_produto from itens_pedido where codigo_movimentacao = {codigo_movimentacao}")
         # Cria um novo objeto Item de Pedido
-        novo_item_pedido = ItemPedido(df_item_pedido.codigo_item_pedido.values[0], df_item_pedido.quantidade.values[0], df_item_pedido.valor_unitario.values[0], pedido, produto)
+        nova_movimentacao = ItemPedido(df_movimentacao.codigo_movimentacao.values[0], df_movimentacao.quantidade.values[0], df_movimentacao.valor_unitario.values[0], pedido, produto)
         # Exibe os atributos do novo Item de Pedido
-        print(novo_item_pedido.to_string())
-        # Retorna o objeto novo_item_pedido para utilização posterior, caso necessário
-        return novo_item_pedido
+        print(nova_movimentacao.to_string())
+        # Retorna o objeto nova_movimentacao para utilização posterior, caso necessário
+        return nova_movimentacao
 
-    def atualizar_item_pedido(self) -> ItemPedido:
+    def atualizar_movimentacao(self) -> ItemPedido:
         # Cria uma nova conexão com o banco que permite alteração
         oracle = OracleQueries(can_write=True)
         oracle.connect()
 
         # Solicita ao usuário o código do item de pedido a ser alterado
-        codigo_item_pedido = int(input("Código do Item de Pedido que irá alterar: "))        
+        codigo_movimentacao = int(input("Código do Item de Pedido que irá alterar: "))        
 
         # Verifica se o item de pedido existe na base de dados
-        if not self.verifica_existencia_item_pedido(oracle, codigo_item_pedido):
+        if not self.verifica_existencia_movimentacao(oracle, codigo_movimentacao):
 
             # Lista os pedido existentes para inserir no item de pedido
             self.listar_pedidos(oracle, need_connect=True)
@@ -90,50 +64,50 @@ class Controller_Movimentacao:
             valor_unitario = float(input(f"Informe o valor unitário do produto {produto.get_descricao()}: "))
 
             # Atualiza o item de pedido existente
-            oracle.write(f"update itens_pedido set quantidade = {quantidade}, valor_unitario = {valor_unitario}, codigo_pedido = {pedido.get_codigo_pedido()}, codigo_produto = {produto.get_codigo()} where codigo_item_pedido = {codigo_item_pedido}")
+            oracle.write(f"update itens_pedido set quantidade = {quantidade}, valor_unitario = {valor_unitario}, codigo_pedido = {pedido.get_codigo_pedido()}, codigo_produto = {produto.get_codigo()} where codigo_movimentacao = {codigo_movimentacao}")
             # Recupera os dados do novo item de pedido criado transformando em um DataFrame
-            df_item_pedido = oracle.sqlToDataFrame(f"select codigo_item_pedido, quantidade, valor_unitario, codigo_pedido, codigo_produto from itens_pedido where codigo_item_pedido = {codigo_item_pedido}")
+            df_movimentacao = oracle.sqlToDataFrame(f"select codigo_movimentacao, quantidade, valor_unitario, codigo_pedido, codigo_produto from itens_pedido where codigo_movimentacao = {codigo_movimentacao}")
             # Cria um novo objeto Item de Pedido
-            item_pedido_atualizado = ItemPedido(df_item_pedido.codigo_item_pedido.values[0], df_item_pedido.quantidade.values[0], df_item_pedido.valor_unitario.values[0], pedido, produto)
+            movimentacao_atualizado = ItemPedido(df_movimentacao.codigo_movimentacao.values[0], df_movimentacao.quantidade.values[0], df_movimentacao.valor_unitario.values[0], pedido, produto)
             # Exibe os atributos do item de pedido
-            print(item_pedido_atualizado.to_string())
+            print(movimentacao_atualizado.to_string())
             # Retorna o objeto pedido_atualizado para utilização posterior, caso necessário
-            return item_pedido_atualizado
+            return movimentacao_atualizado
         else:
-            print(f"O código {codigo_item_pedido} não existe.")
+            print(f"O código {codigo_movimentacao} não existe.")
             return None
 
-    def excluir_item_pedido(self):
+    def excluir_movimentacao(self):
         # Cria uma nova conexão com o banco que permite alteração
         oracle = OracleQueries(can_write=True)
         oracle.connect()
 
         # Solicita ao usuário o código do item de pedido a ser alterado
-        codigo_item_pedido = int(input("Código do Item de Pedido que irá excluir: "))        
+        codigo_movimentacao = int(input("Código do Item de Pedido que irá excluir: "))        
 
         # Verifica se o item de pedido existe na base de dados
-        if not self.verifica_existencia_item_pedido(oracle, codigo_item_pedido):            
+        if not self.verifica_existencia_movimentacao(oracle, codigo_movimentacao):            
             # Recupera os dados do novo item de pedido criado transformando em um DataFrame
-            df_item_pedido = oracle.sqlToDataFrame(f"select codigo_item_pedido, quantidade, valor_unitario, codigo_pedido, codigo_produto from itens_pedido where codigo_item_pedido = {codigo_item_pedido}")
-            pedido = self.valida_pedido(oracle, df_item_pedido.codigo_pedido.values[0])
-            produto = self.valida_produto(oracle, df_item_pedido.codigo_produto.values[0])
+            df_movimentacao = oracle.sqlToDataFrame(f"select codigo_movimentacao, quantidade, valor_unitario, codigo_pedido, codigo_produto from itens_pedido where codigo_movimentacao = {codigo_movimentacao}")
+            pedido = self.valida_pedido(oracle, df_movimentacao.codigo_pedido.values[0])
+            produto = self.valida_produto(oracle, df_movimentacao.codigo_produto.values[0])
             
-            opcao_excluir = input(f"Tem certeza que deseja excluir o item de pedido {codigo_item_pedido} [S ou N]: ")
+            opcao_excluir = input(f"Tem certeza que deseja excluir o item de pedido {codigo_movimentacao} [S ou N]: ")
             if opcao_excluir.lower() == "s":
                 # Revome o produto da tabela
-                oracle.write(f"delete from itens_pedido where codigo_item_pedido = {codigo_item_pedido}")                
+                oracle.write(f"delete from itens_pedido where codigo_movimentacao = {codigo_movimentacao}")                
                 # Cria um novo objeto Item de Pedido para informar que foi removido
-                item_pedido_excluido = ItemPedido(df_item_pedido.codigo_item_pedido.values[0], df_item_pedido.quantidade.values[0], df_item_pedido.valor_unitario.values[0], pedido, produto)
+                movimentacao_excluido = ItemPedido(df_movimentacao.codigo_movimentacao.values[0], df_movimentacao.quantidade.values[0], df_movimentacao.valor_unitario.values[0], pedido, produto)
                 # Exibe os atributos do produto excluído
                 print("Item do Pedido Removido com Sucesso!")
-                print(item_pedido_excluido.to_string())
+                print(movimentacao_excluido.to_string())
         else:
-            print(f"O código {codigo_item_pedido} não existe.")
+            print(f"O código {codigo_movimentacao} não existe.")
 
-    def verifica_existencia_item_pedido(self, oracle:OracleQueries, codigo:int=None) -> bool:
+    '''def verifica_existencia_movimentacao(self, oracle:OracleQueries, codigo:int=None) -> bool:
         # Recupera os dados do novo pedido criado transformando em um DataFrame
-        df_pedido = oracle.sqlToDataFrame(f"select codigo_item_pedido, quantidade, valor_unitario, codigo_pedido, codigo_produto from itens_pedido where codigo_item_pedido = {codigo}")
-        return df_pedido.empty
+        df_pedido = oracle.sqlToDataFrame(f"select codigo_movimentacao, quantidade, valor_unitario, codigo_pedido, codigo_produto from itens_pedido where codigo_movimentacao = {codigo}")
+        return df_pedido.empty'''
 
     def listar_contas(self, oracle:OracleQueries, need_connect:bool=False):
         query = """
@@ -148,17 +122,6 @@ class Controller_Movimentacao:
             oracle.connect()
         print(oracle.sqlToDataFrame(query))
 
-    def listar_produtos(self, oracle:OracleQueries, need_connect:bool=False):
-        query = """
-                select prd.codigo_produto
-                    , prd.descricao_produto 
-                from produtos prd
-                order by prd.descricao_produto 
-                """
-        if need_connect:
-            oracle.connect()
-        print(oracle.sqlToDataFrame(query))
-
     def valida_conta(self, oracle:OracleQueries, pnumero:int=None) -> Contas:
         if self.ctrl_conta.verifica_existencia_conta(oracle, pnumero):
             print(f"A Conta {pnumero} informada não existe na base.")
@@ -167,20 +130,6 @@ class Controller_Movimentacao:
             oracle.connect()
             # Recupera os dados do novo cliente criado transformando em um DataFrame
             df_pedido = oracle.sqlToDataFrame(f"select codigo_pedido, data_pedido, cpf, cnpj from pedidos where codigo_pedido = {codigo_pedido}")
-            cliente = self.ctrl_pedido.valida_cliente(oracle, df_pedido.cpf.values[0])
-            fornecedor = self.ctrl_pedido.valida_fornecedor(oracle, df_pedido.cnpj.values[0])
             # Cria um novo objeto cliente
             pedido = Pedido(df_pedido.codigo_pedido.values[0], df_pedido.data_pedido.values[0], cliente, fornecedor)
             return pedido
-
-    def valida_produto(self, oracle:OracleQueries, codigo_produto:int=None) -> Produto:
-        if self.ctrl_produto.verifica_existencia_produto(oracle, codigo_produto):
-            print(f"O produto {codigo_produto} informado não existe na base.")
-            return None
-        else:
-            oracle.connect()
-            # Recupera os dados do novo produto criado transformando em um DataFrame
-            df_produto = oracle.sqlToDataFrame(f"select codigo_produto, descricao_produto from produtos where codigo_produto = {codigo_produto}")
-            # Cria um novo objeto Produto
-            produto = Produto(df_produto.codigo_produto.values[0], df_produto.descricao_produto.values[0])
-            return produto
